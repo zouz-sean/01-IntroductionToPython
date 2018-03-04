f884e7f10b2030e021255bf323286ca948fd7ea2
DavidMutchler <mutchler@rose-hulman.edu>
2018-03-04 18:08:46 -0500

    Minor changes to the instructions (no changes in content).

--- src/m2_hello_world.py
+++ src/m2_hello_world.py
@@ -1,14 +1,18 @@
 print('Hello, World')
 
 ###############################################################################
-# This line is a COMMENT -- a note to human readers of this file.
-# When a program runs, it ignores everything from a  # (hash)  mark
-# to the end of the line with the  #  mark.
 #
-# We call files that have Python code in them MODULES.  Line 1 of this
-# module (look at it now) prints onto the Run tab the STRING
-#   Hello, World
-# Anything surrounded by quote marks (single or double) is a STRING.
+#   This line is a COMMENT -- a note to human readers of this file.
+#   When a program runs, it ignores everything from a  # (hash)  mark
+#   to the end of the line with the  #  mark.
+#
+#   We call files that have Python code in them MODULES.
+#   Line 1 of this module (look at it now) prints (displays) the STRING
+#      Hello, World
+#   on the Run window (to the right).
+#
+#   Anything surrounded by quote marks (single or double) is a STRING.
+#
 ###############################################################################
 
 ###############################################################################
@@ -16,11 +20,13 @@ print('Hello, World')
 # TODO: 1.
 #   (Yes, that means for YOU to DO things per these instructions:)
 #
-#   Run this module by right clicking anywhere in this window and select
+#   Run this module by right clicking anywhere in this window and selecting
 #        Run 'name of file'
-#   After running, find the Console tab (below) and confirm that
+#   Or, use the keyboard shortcut:  Shift F10
+#
+#   After running, find the Run window (to the right) and confirm that
 #        Hello, World
-#   did indeed get printed (displayed) on the Console.
+#   did indeed get printed (displayed) on that window.
 #
 ###############################################################################
 
@@ -28,23 +34,23 @@ print('Hello, World')
 #
 # TODO: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
-#   on the right.  Each blue bar indicates a TO DO in this module.
+#   on the right.  Each blue bar indicates a thing TO-DO in this module.
 #
-#     a. You can use the blue bars to go from one TO DO to the next
+#     a. You can use the blue bars to go from one TO-DO to the next
 #        by clicking on the blue bars.  ** Try that now. **
 #
-#     b. When you have completed a TO DO, you should change the word
-#          TODO
+#     b. When you have completed a TO-DO, you should change the word
+#          _TODO_      (ignore the underscores on this line)
 #        to
-#          DONE.
-#        Try it now on line 16 above, and note that its blue bar on
+#          DONE
+#        Try it now on line 20 above, and note that its blue bar on
 #        the scrollbar-like thing to the right has gone away.
 #
 # If you change TODOs to DONEs like this, you can tell when you have
 # finished all the exercises in a module -- there will be no blue bars
 # left on the scrollbar-like thing to the right.
 #
-# You have now completed TO DO #2, so change its TO DO on line 29 to DONE
+# You have now completed TO-DO #2, so change its TO-DO on line 35 to DONE
 # (and proceed similarly for all forthcoming TODOs in this course).
 #
 ###############################################################################
@@ -52,11 +58,13 @@ print('Hello, World')
 ###############################################################################
 #
 # TODO: 3.
-#   Add another   print   statement below.
+#   Add another   print   statement below the current Line 1 above.
 #   It should print any string that you want (but keep it G-rated!)
-#   Test your code by re-running this module using either the right click
-#       method again or by using the play button in the upper right.
-#   Look at the Console to be sure that your string printed as expected.
+#
+#   Test your code by re-running this module, either by proceeding as you did
+#   when you ran this module the first time, or by pressing the green-triangle
+#   "Play" button that is on the toolbar at the top of this window.
+#   Look at the Run window to be sure that your string printed as expected.
 #
 ###############################################################################
 
@@ -69,7 +77,7 @@ print('Hello, World')
 #   You do NOT have to use strings for this, so no quotation marks!
 #
 #   TEST your code by re-running this module, then asking someone
-#   whom you trust:  What number did your   print   display for TO DO 4?
+#   whom you trust:  What number did your   print   display for this TO-DO?
 #   (HINT: It is an INTERESTING number.)  Get help if your value is wrong.
 #
 ###############################################################################
@@ -77,25 +85,39 @@ print('Hello, World')
 ###############################################################################
 #
 # TODO: 5.
-#   Look at the list of files in this project to the left.
-#   Note that this file (m2_hello_world.py) is now displayed in a blue
-#   font color (if the file is highlighted select a different file so yu can
-#   see the blue font color).  That means that you have made changes to
-#   this file which have not yet been committed.
+#   Look at the list of files in the Project window (to the left).  Note that
+#   this file (m2_hello_world.py) is now displayed in a blue font color.
+#     - Clicking on a different file makes it easier to see
+#       the blue font color of this file.
 #
-#   COMMIT your work by selecting VCS from the menu bar, then select Commit Changes
-#   Make sure only the files you want to commit are checked and optionally
-#   add a quick Commit message to describe your work.  Then hover over the
-#   Commit button and select Commit and Push.  Commit saves the work to
-#   your computer.  "and Push" saves a copy of your work up into your Github
-#   repository (saving to the cloud is a better way to permanently safe work).
+#   The blue font color means that you have made changes to this file which
+#   have not yet been COMMITTED to version control and PUSHED to the cloud.
+#
+#   COMMIT and PUSH your work by:
+#     1. Select   VCS   from the menu bar (above).
+#     2. Choose  Commit  from the pull-down menu that appears.
+#     3. In the  Commit Changes  window that pops up:
+#          - HOVER over the  Commit  button
+#              (in the lower-right corner of the window)
+#          - CLICK on  Commit and Push.
+#
+#   COMMIT adds the changed work to the version control on your computer
+#   and PUSH adds the changed work into your Github repository in the "cloud".
+#
+#   Always PUSH (in addition to the COMMIT) so that your work is backed-up
+#   in the cloud, and also so that your instructor can access it as needed.
+#
+#   In this course, you will always want to COMMIT-and-PUSH ALL your changed
+#   files, so you can safely leave the check marks unchanged in the pop-up
+#   window.  You can optionally add a quick  Commit  message to describe your
+#   work, but that will not be important until you work with teammates.
 #
 #   Oh, one more thing:
-#   Do you have any blue bars left on the on the scrollbar-like thing
-#   to the right?  If so, click on each blue bar and change
-#   its TO DO to DONE.  Then run the file (to make sure you didn't break
-#   anything) then Commit and Sync again.
+#     Do you have any blue bars on the scrollbar-like thing to the right?
+#     If so, click on each blue bar and change its TO-DO to DONE,
+#     then run the file (to make sure you didn't break anything)
+#     and COMMIT-and-PUSH again.
 #
-#   You can COMMIT as often as you like.  DO FREQUENT COMMITS.
+#   You can COMMIT-and-PUSH as often as you like.  DO IT FREQUENTLY.
 #
 ###############################################################################
