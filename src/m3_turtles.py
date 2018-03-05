d7fc366bd667408554d0522ead0755ee77efb119
DavidMutchler <mutchler@rose-hulman.edu>
2018-03-04 21:45:22 -0500

    Minor changes to the instructions.  No changes in content, except:   1. Requires student to construct and use a Pen object (in addition to a SimpleTurtle object).   2. Modifies the COMMIT-and-PUSH instructions to suggest leaving the check marks on the files alone (i.e., commit and push all changed files).   3. Indicates using the style rule of beginning names with a LOWER-case letter.

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -12,74 +12,102 @@ Also:
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
          their colleagues and PUT_YOUR_NAME_HERE.
 """
-########################################################################
+###############################################################################
 #
 # TODO: 1.
-#  (Yes, that means for YOU to DO things per these instructions:)
+#   Yes, that means for YOU to DO things per the following instructions:
 #
-# On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
+#   On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
 #
-# BTW, the top block of text above forms what is called a DOC-STRING.
-# It documents what this module does, in a way that exterior programs
-# can make sense of.  It has no other effect on this program.
+#   BTW, the top block of text above forms what is called a DOC-STRING.
+#   It documents what this module does, in a way that exterior programs
+#   can make sense of.  It has no other effect on this program.
 #
-########################################################################
+###############################################################################
 
 import rosegraphics as rg
 
-########################################################################
+###############################################################################
 #
 # TODO: 2.
-#   Allow this file to use the rosegraphics.py file by marking the src
-#   directory as a "Sources Root".  Do that by right clicking on the src folder,
-#   then selector  Mark Directory As --> Sources Root
-#   Watch the red underlines on the line of code above disappear as you do that step.
-#   You will do that once for every project that uses rosegraphics so get used to it. :)
+#   Allow this module to use the  rosegraphics.py  module by marking the
+#     src
+#   folder in this project as a "Sources Root", as follows:
 #
-#   Run this module by Right clicking in this window and select Run 'filename'
-#   A window will pop up and Turtles will move around.  After the
-#   Turtles stop moving, *click anywhere in the window to close it*.
+#     In the Project window (to the left), right click on the src  folder,
+#     then select  Mark Directory As  ~  Sources Root.
 #
-#   Then look at the code below.  Raise your hand as you have questions
-#   about what the code is doing.
+#   You will see that  rosegraphics  in the  import  statement above (line 28)
+#   is no longer marked as an error.  You will do this
+#      Select the  src  folder, then  Mark Directory As ~ Sources Root
+#   in all projects that use rosegraphics, so get used to it. :)
 #
-########################################################################
+#   Once  rosegraphics  in the  import  statement is no longer marked
+#   as an error, change the above TO-DO to DONE and coninue to the next TO-DO.
+#
+###############################################################################
 
-# ----------------------------------------------------------------------
-# Set up a   TurtleWindow   object for animation.  The definition of a
-#     TurtleWindow is in the   rg  (shorthand for rosegraphics) module.
-# ----------------------------------------------------------------------
+###############################################################################
+#
+# TODO: 3.
+#   Run this module.  A window will pop up and Turtles will move around.
+#   After the Turtles stop moving, *click anywhere in the window to close it*.
+#
+#   Then look at the code below.  Raise your hand when you have questions about
+#   what the code is doing.  Be sure that you understand the notations for:
+#     -- CONSTRUCTING an instance of a CLASS, e.g.
+#           rg.SimpleTurtle()
+#     -- ASSIGNING the resulting OBJECT (instance of a class) a NAME, e.g.
+#           dave = rg.SimpleTurtle()
+#     -- Applying a METHOD to an object to make the object DO something, e.g.
+#           dave.forward(100)
+#     -- Accessing an INSTANCE VARIABLE of an object, e.g.
+#           dave.speed = 10
+#           print(matt.speed)
+#
+#   After you are confident that you understand all the code below,
+#   change this TO-DO to DONE and continue to the next TOD-DO.
+#
+###############################################################################
+
+# -----------------------------------------------------------------------------
+# CONSTRUCT (make) a   TurtleWindow   object and set it up for animation.
+# The definition of a  TurtleWindow  is in the   rg  (shorthand for
+# rosegraphics) module.
+# -----------------------------------------------------------------------------
 window = rg.TurtleWindow()
 window.delay(20)  # Bigger numbers mean slower animation.
 
-# ----------------------------------------------------------------------
-# Makes (constructs) a   SimpleTurtle   object.
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# CONSTRUCT (make) a  SimpleTurtle  object and ASSIGN a NAME to the object.
+# -----------------------------------------------------------------------------
 dave = rg.SimpleTurtle()
 
-# ----------------------------------------------------------------------
-# Ask the SimpleTurtle objects to do things:
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Ask the SimpleTurtle object to do things by applying METHODs to it:
+# -----------------------------------------------------------------------------
 dave.forward(100)
 dave.left(90)
 dave.forward(200)
 
-# ----------------------------------------------------------------------
-# Construct a new turtle and ask it to do things.
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Construct a second SimpleTurtle,
+# set its  pen  and  speed  INSTANCE VARIABLES, and ask it to do things.
+# -----------------------------------------------------------------------------
 matt = rg.SimpleTurtle('turtle')
-matt.pen = rg.Pen('red', 30)
+matt.pen = rg.Pen('red', 30)  # Second argument is the Pen's thickness
 matt.speed = 10  # Faster
+
 matt.backward(50)
-matt.left(90)
+matt.right(90)
 matt.forward(50)
 
 
-########################################################################
+###############################################################################
 #
-# TODO: 3.
-#   Add a few more line of your own code above to make one of the
-#   existing Turtles move some more and/or have different
+# TODO: 4.
+#   Add a few more line of your own code to make one of the
+#   existing SimpleTurtles move some more and/or have different
 #   characteristics.
 #
 #      ** Nothing fancy is required. **
@@ -87,51 +115,57 @@ matt.forward(50)
 #
 #   As always, test by running the module.
 #
-########################################################################
+###############################################################################
 
-########################################################################
+###############################################################################
 #
-# TODO: 4.
-#   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
+# TODO: 5.
+#   The above code  CONSTRUCTS  two SimpleTurtle objects
+#   and gives those objects NAMES:
 #       dave    matt
 #
-#   Below this TO DO comment construct another SimpleTurtle object,
-#       naming it whatever you want.
-#   Names cannot have spaces or special characters, but they can have
-#   digits and underscores like     this_1_has   (get it?).
+#   Add code of your own that constructs another SimpleTurtle object,
+#   naming it whatever you want.  Names cannot have spaces or special
+#   characters, but they can have digits and underscores, e.g.
+#      this_1_has
 #
-#   After you construct a SimpleTurtle, add a few more lines that
-#   make YOUR SimpleTurtle move around a bit.
+#   STYLE RULE: Your names should always begin with a LOWER_CASE letter.
+#   So   mary   is OK but   Mary   is NOT OK.
+#
+#   Then add a few more lines that:
+#     -- Constructs a Pen object,
+#     -- assigns your SimpleTurtle's  pen  to the constructed Pen object, and
+#     -- makes your SimpleTurtle move around a bit.
 #
 #      ** Nothing fancy is required. **
 #      ** A SUBSEQUENT exercise will let you show your creativity. **
 #
 #   As always, test by running the module.
 #
-########################################################################
+###############################################################################
 
-########################################################################
+
+###############################################################################
 #
-# TODO: 5.
+# TODO: 6.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
-#   Then COMMIT and Push your work using the VCS menu option.
+#   Then COMMIT-and-PUSH your work as before:
+#     1. Select   VCS   from the menu bar (above).
+#     2. Choose  Commit  from the pull-down menu that appears.
+#     3. In the  Commit Changes  window that pops up:
+#          - HOVER over the  Commit  button
+#              (in the lower-right corner of the window)
+#          - CLICK on  Commit and Push.
 #
-#   Reminder of those steps...
-#   COMMIT your work by selecting VCS from the menu bar, then select Commit Changes
-#   Make sure only the files you want to commit are checked and optionally
-#   add a quick Commit message to describe your work.  Then hover over the
-#   Commit button and select Commit and Push.  Commit saves the work to
-#   your computer.  "and Push" saves a copy of your work up into your Github
-#   repository (saving to the cloud is a better way to permanently safe work).
+#   You can COMMIT-and-PUSH as often as you like.  DO IT FREQUENTLY.
 #
-#   You can COMMIT as often as you like.  DO FREQUENT COMMITS.
-#
-########################################################################
+###############################################################################
 
-# ----------------------------------------------------------------------
-# This line keeps the window open until the user clicks in the window:
-# Throughout this exercise, close_on_mouse_click should be the last line in the file.
-# ----------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# The next line keeps the window open until the user clicks in the window.
+# Throughout this exercise, this  close_on_mouse_click   line should be the
+# LAST line in the file.  DO NOT ADD CODE BELOW THIS LINE!
+# -----------------------------------------------------------------------------
 window.close_on_mouse_click()
