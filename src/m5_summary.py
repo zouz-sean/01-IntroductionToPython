605df186770dbdbb9e0778c2c0b6d92ac6d4085a
DavidMutchler <mutchler@rose-hulman.edu>
2018-03-04 22:31:43 -0500

    A new, short exercise to summarize the lessons of this Session.

---
+++ src/m5_summary.py
@@ -0,0 +1,30 @@
+"""
+An exercise that summarizes what you have learned in this Session.
+
+Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
+         their colleagues and PUT_YOUR_NAME_HERE.
+"""
+########################################################################
+# TODO: 1.
+#   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
+########################################################################
+
+########################################################################
+# TODO: 2.
+#   Write code that constructs a SimpleTurtle with a red Pen
+#   and makes it move around a bit.  Don't forget to:
+#     -- import rosegraphics and construct a TurtleWindow
+#          at the BEGINNING of your code, and to
+#     -- ask your TurtleWindow to   close_on_mouse_click
+#          as the LAST line of your code.
+#   See the beginning and end of m4e_loopy_turtles for an example.
+#
+#      ** Nothing fancy is required. **
+#      ** The NEXT exercise will let you show your creativity. **
+#
+#   As always, test by running the module.
+#
+#   As always, COMMIT-and-PUSH when you are done with this module.
+#
+###############################################################################
+
