c097f0eaa758930e5bd6731dda1279227872c7c6
zouz-sean <zouz@rose-hulman.edu>
2018-03-05 09:22:20 -0500

    m3_turtles

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -161,7 +161,7 @@ sean.forward(100)
 
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
